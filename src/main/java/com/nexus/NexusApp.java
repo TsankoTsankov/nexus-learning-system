c1d08fa8c3b37c10f9f864042a59897b3dae8d48
TsankoTsankov <tzanesh@gmail.com>
2026-02-01 01:04:05 +0200

    Feature: A successfulul PDF fil crawler

--- src/main/java/com/nexus/NexusApp.java
+++ src/main/java/com/nexus/NexusApp.java
@@ -1,10 +1,31 @@
 package com.nexus;
 
-/**
- * Hello world!
- */
+import java.util.List;
+import java.util.Scanner;
+
 public class NexusApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        LibraryService libraryService = new LibraryService();
+
+        System.out.println("===================================");
+        System.out.println("     Nexus: The Knowledge Core     ");
+        System.out.println("===================================");
+
+        System.out.println("Enter the PATH to the folder to be scanned: ");
+        String pathInput = scanner.nextLine();
+
+        if (pathInput.isBlank()){
+            System.out.println("Error: Please provide apath. Exiting...");
+            return;
+        }
+        System.out.println("Scanning '" + pathInput + "'...");
+        List<Book> books = libraryService.findBooksInDirectory(pathInput);
+        System.out.println("Found " + books.size() +" PDF files");
+
+        for (Book book : books) {
+            System.out.println(" - " + book.getTitle());
+        }
+        System.out.println("===================================");
     }
 }
